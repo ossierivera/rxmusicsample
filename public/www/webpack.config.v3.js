3054f835b36563b5231b079a8a49c35ac6a74816
Ossie Rivera <os.rivera@gmail.com>
2019-06-06 17:02:55 -0400

    Added some screenshots

--- public/www/webpack.config.v3.js
+++ public/www/webpack.config.v3.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 // Knowing about webpack or a similar tool and knowing how to setup
-// the config file is above avarage for a junior
+
 module.exports = {
   entry: path.resolve(__dirname, "test.v3.js"),
   output: {
